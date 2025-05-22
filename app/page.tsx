94c7acbde9b5fbec3af8c5b13ea7544002c27da4
wtan116 <wtan116@gmail.com>
2025-05-21 22:10:03 -0700

    Updated homepage title for deployment test

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
     <>
       <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
+        <h2 className="font-medium text-xl mb-4">Welcome to Flowmenta-Videogen 🚀</h2>
         {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
       </main>
     </>
